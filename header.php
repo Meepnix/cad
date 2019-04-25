717405a4df9c881e6605b5f45d3deff04e377471
Christopher Massey <moonix.uk@gmail.com>
2019-04-25 03:02:02 +0100

    nav format and social buttons

--- header.php
+++ header.php
@@ -10,14 +10,13 @@
 
         <title>Citizens Advice Darlington</title>
 
-        
-
-        
-
         <?php wp_head();?>
 
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <!-- Font Awesome CSS -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
         <!-- Theme stylesheet -->
         <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
@@ -28,10 +27,10 @@
     <div class="container-fluid" style="background-color: #D4E5EF;">
 
         <div class="row">
-            <div class="col">
+            <div class="col-1">
       
             </div>
-            <div class="col-10">
+            <div class="col-9">
 
             <!-- Logo -->
             <?php 
@@ -43,7 +42,33 @@
             <a class="navbar-brand" href="#"><img src="<?php echo $image[0]; ?>" alt=""></a>
       
             </div>
-            <div class="col">
+            <div class="col-2">
+                <div class="row">
+                    <div class="col text-center">
+
+                        <div class="social">
+
+                        
+                            <a href="http://facebook.com/darlingtoncab" title="Follow us on Facebook." target="_blank">
+                            <i class="fab fa-facebook fa-3x"></i>
+                            </a>
+
+                            <a href="http://twitter.com/darlingtoncab" title="Follow us on Twitter." target="_blank">
+                            <i class="fab fa-twitter-square fa-3x"></i>
+                            </a>
+
+                            <a href="https://www.instagram.com/darlingtoncad" title="Follow us on Instagram." target="_blank">
+                            <i class="fab fa-instagram fa-3x"></i>
+                            </a>    
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-center">
+                    <a href="#" class="btn btn-donate">DONATE</a> 
+                    </div>
+                </div>
+
       
             </div>
         </div>
@@ -57,9 +82,6 @@
         <nav class="navbar sticky-top navbar-expand-md navbar-dark" style="background-color: #004B88;" role="navigation">
             <div class="container">
                 
-
-                
-
 	            <!-- Brand and toggle get grouped for better mobile display -->
 	            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
 		            <span class="navbar-toggler-icon"></span>
@@ -73,7 +95,7 @@
                     'container'         => 'div',
                     'container_class'   => 'collapse navbar-collapse',
                     'container_id'      => 'bs-example-navbar-collapse-1',
-                    'menu_class'        => 'nav navbar-nav',
+                    'menu_class'        => 'nav navbar-nav header-menu',
                     'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
                     'walker'            => new WP_Bootstrap_Navwalker(),
                 ) );
